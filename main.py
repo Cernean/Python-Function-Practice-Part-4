7fcfb1d433a5cc3eb1ef2a9c7a53c57d1f0a899e
Constantin Cernean <ccernean@yahoo.com.com>
2023-06-05 18:14:26 -0400

    First 4 activities

--- main.py
+++ main.py
@@ -37,33 +37,3 @@ def num_within(a, b, c):
 print(num_within(1,2,3))     
 print(num_within(3,2,1))     
 print(num_within(6,7,8))
-
-
-
-triangle = [[1],[1,1]]
-def pascal(n):
-  if n < 1:
-    print("invalid number of rows")
-  elif n == 1:
-    print(triangle[0])
-  else:
-    row_number = 2
-    while len(triangle) < n:
-      row = []
-      row_prev = triangle[row_number - 1]
-      length = len(row_prev)+1
-      for i in range(length):
-        if i == 0:
-          row.append(1)
-        elif i > 0 and i < length-1:
-          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
-        else:
-          row.append(1)
-      triangle.append(row)
-      row_number += 1
-
-    for row in triangle:
-      print(row)
-
-pascal(2)
-pascal(5)
