e5ea82e6840b888fad51323442e0ca8e2e4e7a53
Constantin Cernean <ccernean@yahoo.com.com>
2023-06-11 20:05:15 -0400

    Static Files and Styling

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,17 +1,22 @@
 # config                    
 from flask import Flask
 
+# factory
 def create_app():
-
     app = Flask(__name__)
 
-    # factory
-    from . import pet
+    # index route
+    @app.route('/')
+    def index(): 
+        return 'Hello, PetFax!'
+    
+    # register pet blueprint 
+    from . import pet 
     app.register_blueprint(pet.bp)
 
-# index route
-    @app.route('/')
-    def index():
-        return 'This is the Home page!'  
-    
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app 
     return app
